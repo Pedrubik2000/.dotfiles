5fa7ede9c9d2a097f33eca5daeb6860ebaf49d12
Pedro Echevarria <pedroechevarria.us@gmail.com>
2024-12-18 23:28:45 -0500

    first commit

---
+++ nvim/.config/nvim/nvim/lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+}
