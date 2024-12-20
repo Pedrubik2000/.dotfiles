bd57333a4ded8ccc333e3f4481364114b1bd5623
Pedro Echevarria <pedroechevarria.us@gmail.com>
2024-12-20 12:14:15 -0500

    Second commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,4 @@
+require("config.init")
 require("config.lazy")
 require("config.lsp_config")
+require("config.telescope")
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,6 +5,25 @@ return {
 	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	{ "Mofiqul/dracula.nvim" },
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
+	"mfussenegger/nvim-lint",
+	"mhartington/formatter.nvim",
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{
+		'barrett-ruth/live-server.nvim',
+		build = 'pnpm add -g live-server',
+		cmd = { 'LiveServerStart', 'LiveServerStop' },
+		config = true
+	}
 }
